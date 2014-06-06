a9a2f2cdafef946975d28714d6e63d80d8d2d746
Bob Kuhar <robertkuhar@gmail.com>
2014-06-05 22:35:23 -0700

    Predicates, baby

--- src/ch04/AssetUtil.java
+++ src/ch04/AssetUtil.java
@@ -1,15 +1,19 @@
 package ch04;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AssetUtil {
     public static int totalAssetValues( final List<Asset> assets ) {
-        int totalAssetValues = assets.stream().mapToInt( asset -> asset.value ).sum();
-        return totalAssetValues;
+        return totalAssetValues( assets, asset -> true );
     }
 
     public static int totalAssetValues( final List<Asset> assets, final Asset.AssetType type ) {
-        int totalAssetValues = assets.stream().filter( asset -> asset.type == type ).mapToInt( asset -> asset.value ).sum();
+        return totalAssetValues( assets, asset -> asset.type == type );
+    }
+
+    public static int totalAssetValues( final List<Asset> assets, final Predicate<Asset> assetSelector ) {
+        int totalAssetValues = assets.stream().filter( assetSelector ).mapToInt( asset -> asset.value ).sum();
         return totalAssetValues;
     }
 }
