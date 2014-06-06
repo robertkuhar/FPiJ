161ea92218af2f287705fc9e254ddfb748558b4e
Bob Kuhar <robertkuhar@gmail.com>
2014-06-05 23:57:10 -0700

    Put the copyright crap everywhere.

--- src/ch04/AssetUtil.java
+++ src/ch04/AssetUtil.java
@@ -1,3 +1,10 @@
+/***
+ * Excerpted from "Functional Programming in Java", published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/vsjava8 for more book information.
+ ***/
 package ch04;
 
 import java.util.List;
