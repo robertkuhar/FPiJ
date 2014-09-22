348a0cf954cc544d187b58c27e7eeae0ed6ee1c3
Bob Kuhar <robertkuhar@gmail.com>
2014-09-21 18:09:30 -0700

    Add WingDing comment

--- src/ch07/Memoizer.java
+++ src/ch07/Memoizer.java
@@ -16,6 +16,14 @@ import java.util.function.BiFunction;
 public class Memoizer {
     public static <T, R> R callMemoized( final BiFunction<Function<T, R>, T, R> function, final T input ) {
         Function<T, R> memoized = new Function<T, R>() {
+            {
+                /*
+                 * WingDing thinks maybe this Map is in the wrong place since
+                 * the Price list was fixed at RodCutterMemoizer why wasn't this
+                 * Map fixed down at that time as well.
+                 */
+                System.out.println( "new Function called" );
+            }
             private final Map<T, R> store = new HashMap<>();
 
             public R apply( final T input ) {
